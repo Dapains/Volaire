c1b8ed8411e326ffa24a8ecfeb93da54c51442dc
Dapains <32205748+Dapains@users.noreply.github.com>
2017-09-28 15:00:46 -0400

    Update Adventure.py

--- Adventure.py
+++ Adventure.py
@@ -1,35 +1,41 @@
-class Item(object):
-    def __init__(self, name, attack, armor, weight, price):
-        self.name = name
-        self.attack = attack
-        self.armor = armor
-        self.weight = weight
-        self.price = price
+#Owen Matyi 9/24/17
+
+#Lists
+Keys = {"HouseKey": False, "GateKey": False, "LockboxKey": False, "DrownedChestKey": False, "RedGem": False, "BlueGem":False, "GreenGem":False, "PurpleGem":False, "AnctientIdol": False }
+
+#starting variables
+life = 100
 
 
-class Inventory(object):
-    def __init__(self):
-        self.items = {}
-
-    def add_item(self, item):
-        self.items[item.name] = item
-
-    def print_items(self):
-        print('\t'.join(['Name', 'Atk', 'Arm', 'Lb', 'Val']))
-        for item in self.items.values():
-            print('\t'.join([str(x) for x in [item.name, item.attack, item.armor, item.weight, item.price]]))
+def changegold(amount):
+    global gold
+    gold += amount
+    print("You gained", amount, "Gold. You now have", gold)
 
 
-inventory = Inventory()
-inventory.add_item(Item('Sword', 5, 1, 15, 2))
-inventory.add_item(Item('Armor', 0, 10, 25, 5))
-inventory.print_items()
+def changelife(hpchange):
+    global life
+    life += hpchange
+    if life <= 0:
+        dead()
+    print("You lost", hpchange, "Life. you have", life, "life left.")
 
 
-print("Welcome to Voltaire! the new fun text based adventure game")
-print("The rules are simple  ")
-Char = input("What is your name?")
-print("Welcome", Char,"Let us begin your adventure!")
+def dead():
+    print("you are dead try again next time!")
+    exit(0)
+
+
+def start():
+    print("Welcome to Voltaire! the new fun text based adventure game")
+    print("The rules are simple  ")
+
+
+def charactercreation():
+    char = input("What is your name?")
+    print("Welcome", char, "Let us begin your adventure!")
+    beginning()
+
 
 def beginning():
     start = input("you are in a large forest")
@@ -40,17 +46,27 @@ def beginning():
         print("you move forward")
     elif start.lower == "loot":
         print("You found some")
+
+
 def forest1():
     f1 = input("You enter a dense forest with many trees around you")
+
+
 def forest2():
     f2 = input()
+
+
 def forest3():
     f3 = input()
+
 
 def outsidehouse():
     out = input("The trees part. You come across a large white house with a lamp and a mail box outside")
 
+
 def cave():
     c = input("you enter a large cave you can hear a running water and other noises further down but you may need to go further down to hear them more clearly")
+
+
 def depths():
     d = input()
